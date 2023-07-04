3c8d1b68412b3057869317798f3ed0c92d940b67
eugene <geymzheka@gmail.com>
2023-07-04 20:08:22 +0500

    add functions main

---
+++ main.py
@@ -0,0 +1,14 @@
+from utils import get_data, delete_empty_operations, get_first_five_sorted_operations, convert_date, convert_number_to, convert_number_from, print_list_operation
+from settings import PATH_WITH_FIXTURES
+
+
+def main():
+    data = get_data(PATH_WITH_FIXTURES)
+    new_data = delete_empty_operations(data)
+    five_operations = get_first_five_sorted_operations(new_data)
+    end_ = print_list_operation(five_operations)
+    print(end_)
+
+
+if __name__ == '__main__':
+    main()

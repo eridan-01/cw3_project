5014ff9e79b99ff9cd01271e26eb63faa1ae7147
eugene <geymzheka@gmail.com>
2023-07-07 00:27:25 +0500

    delete

--- tests/test_cw.py
+++ tests/test_cw.py
@@ -1,10 +1,9 @@
 from settings import PATH_WITH_FIXTURES
-from utils import get_data, delete_empty_operations, get_first_five_sorted_operations, print_list_operation, convert_date
+from utils import get_data, delete_empty_operations, get_first_five_sorted_operations, convert_date
 
 data = get_data(PATH_WITH_FIXTURES)
 new_data = delete_empty_operations(data)
 five_operations = get_first_five_sorted_operations(new_data)
-end_ = print_list_operation(five_operations)
 
 
 def test_get_data():

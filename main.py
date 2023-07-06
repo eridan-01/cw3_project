935cf93657e2006805dc4e0fa9fd1acf1dd63215
eugene <geymzheka@gmail.com>
2023-07-07 00:33:36 +0500

    get results

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from utils import get_data, delete_empty_operations, get_first_five_sorted_operations, print_list_operation
 from settings import PATH_WITH_FIXTURES
+from utils import get_data, delete_empty_operations, get_first_five_sorted_operations, get_results
 
 
 def main():
     data = get_data(PATH_WITH_FIXTURES)
     new_data = delete_empty_operations(data)
     five_operations = get_first_five_sorted_operations(new_data)
-    end_ = print_list_operation(five_operations)
+    print_list_operation = get_results(five_operations)
     print(end_)
 
 
